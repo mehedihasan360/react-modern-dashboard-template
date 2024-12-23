dafffeb5483cb088b526a209ab3698519bf69a88
mehedihasan360 <mehedihasan.m360ict@gmail.com>
2024-12-23 10:32:07 +0600

    first commit

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,19 +1,20 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import { persistReducer, persistStore } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import api from "./api/api";
-import authSlice from "./slice/authSlice";
-import drawerSlice from "./slice/drawerSlice";
-import modalSlice from "./slice/modalSlice";
-import notificationSlice from "./slice/notificationSlice";
-import themeSlice from "./slice/themeSlice";
-import { errorMiddleware, successMiddleware } from "./utilities/middleware";
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import api from './api/api';
+import authSlice from './slice/authSlice';
+import drawerSlice from './slice/drawerSlice';
+import modalSlice from './slice/modalSlice';
+import notificationSlice from './slice/notificationSlice';
+import themeSlice from './slice/themeSlice';
+import filterSlice from './slice/filterSlice';
+import { errorMiddleware, successMiddleware } from './utilities/middleware';
 
 const persistConfig = {
-  key: "ROOT_WEB_NAME",
+  key: 'ROOT_WEB_NAME',
   storage,
-  whitelist: ["auth", "theme"],
+  whitelist: ['auth', 'theme', 'filter'],
   version: 1,
   blacklist: [api.reducerPath],
 };
@@ -23,7 +24,7 @@ const rootReducer = combineReducers({
   theme: themeSlice,
   modal: modalSlice,
   drawer: drawerSlice,
-  // filter: filterSlice,
+  filter: filterSlice,
   notification: notificationSlice,
   [api.reducerPath]: api.reducer,
 });
@@ -36,7 +37,7 @@ const store = configureStore({
     getDefaultMiddleware({
       serializableCheck: false,
     }).concat(api.middleware, successMiddleware, errorMiddleware),
-  devTools: process.env.NODE_ENV === "development",
+  devTools: process.env.NODE_ENV === 'development',
 });
 
 export const persistor = persistStore(store);
--- src/utilities/helper.ts
+++ src/utilities/helper.ts
@@ -1,5 +1,5 @@
-import dayjs from "dayjs";
-import { image_host_url } from "./images";
+import dayjs from 'dayjs';
+import { image_host_url } from './images';
 
 // TEXT TRUNCATE
 export const truncateText = (
@@ -11,7 +11,7 @@ export const truncateText = (
     return null;
   }
   return text.length > effectiveLength
-    ? text.slice(0, effectiveLength).concat("...")
+    ? text.slice(0, effectiveLength).concat('...')
     : text;
 };
 
@@ -19,18 +19,18 @@ export const truncateText = (
 export const dateAndTimeFormatter = (
   date: string | undefined,
   format:
-    | "DD MMM YYYY"
-    | "DD MMM YYYY, hh:mm A"
-    | "DD/MM/YYYY"
-    | "ddd, Do MMM YYYY"
-    | "DD/MM/YYYY, hh:mm A" = "DD MMM YYYY"
+    | 'DD MMM YYYY'
+    | 'DD MMM YYYY, hh:mm A'
+    | 'DD/MM/YYYY'
+    | 'ddd, Do MMM YYYY'
+    | 'DD/MM/YYYY, hh:mm A' = 'DD MMM YYYY'
 ) => {
-  return date ? dayjs(date).format(format) : "N/A";
+  return date ? dayjs(date).format(format) : 'N/A';
 };
 
 // YAER CONCAT WITH DATE AND MONTH
 export const yearConcatWithDate = (year: number) => {
-  return dayjs(dayjs().format("MM-DD").concat(`-${year}`));
+  return dayjs(dayjs().format('MM-DD').concat(`-${year}`));
 };
 
 // NUMBER FORMAT TO FIXED DECIMAL
@@ -45,38 +45,32 @@ export const formatImageField = (image: string) => {
   if (!image) return [];
   return [
     {
-      uid: "-1",
+      uid: '-1',
       name: image,
-      status: "done",
+      status: 'done',
       url: `${image_host_url}${image}`,
     },
   ];
 };
 
 export const hexToRgba = (hex: string | undefined, opacity: number): string => {
-  if (typeof hex !== "string" || typeof opacity !== "number") {
-    throw new Error("Invalid input: hex must be a string and opacity a number");
+  if (typeof hex !== 'string' || typeof opacity !== 'number') {
+    throw new Error('Invalid input: hex must be a string and opacity a number');
   }
-
-  const normalizedHex = hex.replace(/^#/, "").toLowerCase();
-
+  const normalizedHex = hex.replace(/^#/, '').toLowerCase();
   if (!/^([0-9a-f]{3}|[0-9a-f]{6})$/.test(normalizedHex)) {
-    throw new Error("Invalid hex color format");
+    throw new Error('Invalid hex color format');
   }
-
   const fullHex =
     normalizedHex.length === 3
       ? normalizedHex
-          .split("")
+          .split('')
           .map((char) => char + char)
-          .join("")
+          .join('')
       : normalizedHex;
-
   const r = parseInt(fullHex.slice(0, 2), 16);
   const g = parseInt(fullHex.slice(2, 4), 16);
   const b = parseInt(fullHex.slice(4, 6), 16);
-
   const validOpacity = Math.min(Math.max(opacity, 0), 1);
-
   return `rgba(${r}, ${g}, ${b}, ${validOpacity})`;
 };
